a69189c98c10214ba14759251052b32d7911f055
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-08-25 23:35:18 +0800

    change to setup.py approach

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='venvat',
+    version='0.2.15',
+    description='A script to activate Python virtual environments.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Kelocker',
+    author_email='thekelvin0fficials@gmail.com',
+    license='MIT',
+    url='https://github.com/Kelocker/venvat.git',
+    project_urls={
+        'Homepage': 'https://github.com/Kelocker/venvat.git',
+        'Issues': 'https://github.com/Kelocker/venvat.git/issues'
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+    'console_scripts': [
+        'venvat=venvat.venvat_runner:main'
+        ]
+    },
+    scripts=['scripts/venvat.ps1'],
+)
